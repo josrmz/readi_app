13582b2d92bc0856524399cf9b991d02dfbdfb7a
Joselin Ramirez <josrmz022@gmail.com>
2025-11-24 00:55:35 -0600

    adding to login

--- lib/views/loginpage.dart
+++ lib/views/loginpage.dart
@@ -5,7 +5,49 @@ class LoginPage extends StatelessWidget {
 
  @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        //brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back,
+          size: 20,
+          color: Colors.black,
+          ),
+        ),
+
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Iniciar Sesion",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 20,),
+                    Text("Inicie sesion en su cuenta"),
+                  ],
+                )
+              ],
+            ))
+          ],
+        ),
+
+
+      ),
+    );
+
   }
   }
